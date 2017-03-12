4d7e058282800d43480550b964c6489484234d25
Arpit Anand <mjarpitanand@gmail.com>
2017-03-13 00:08:38 +0530

    Initial commit

---
+++ resources/js/init.js
@@ -0,0 +1,14 @@
+/*Open nav script*/
+function toggle(){
+    document.getElementById('sidebar-wrapper').style.width = '250px';
+
+}
+/*End Of Open nav script*/
+
+/*Close of nav script*/
+
+function toggle_close(){
+    console.log('Close');
+    document.getElementById('sidebar-wrapper').style.width = '0px';
+}
+/*End of close nav script*/
